febf61ace9970af81d2062dd40fb13b247fbce9a
rnpatel9789 <rnpatel9789@gmail.com>
2018-09-13 05:31:53 +0000

    Implemented Tweeter call on tech-news page

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
   
 end
