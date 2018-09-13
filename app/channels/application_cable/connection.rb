59165f45962b4544c57caee7468c243edba18b8b
rnpatel9789 <rnpatel9789@gmail.com>
2018-09-13 19:47:06 +0000

    fixed bug in connection file for comments

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,8 +1,7 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
-    
-    def guest_user 
+    def guest_user
       guest = GuestUser.new
       guest.id = guest.object_id
       guest.name = "Guest User"
@@ -10,20 +9,18 @@ module ApplicationCable
       guest.last_name = "User"
       guest.email = "guest@user.com"
       guest
-    end 
-    
-    def connect 
+    end
+    def connect
       self.current_user = find_verified_user || guest_user
       logger.add_tags 'ActionCable', current_user.email
       logger.add_tags 'ActionCable', current_user.id
     end
     
-    protected 
-    
+    protected
     def find_verified_user
       if verified_user = env['warden'].user
         verified_user
       end
-    end 
+    end
   end
 end
