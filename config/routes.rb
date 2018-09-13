e46af389065e7cd82680f4863b6bec3496c250ae
rnpatel9789 <rnpatel9789@gmail.com>
2018-09-13 18:24:34 +0000

    Created comment broadcast and configure comment routes and updated comment model with callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :comments
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do 
     put :sort, on: :collection  
   end 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  
-  # get 'pages/home'
-  # get 'pages/about'
-  # get 'pages/contact'
   
   resources :blogs do 
     member do 
@@ -17,11 +12,10 @@ Rails.application.routes.draw do
     end 
   end 
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
   
+  mount ActionCable.server => '/cable'
+  root to: 'pages#home'
 end
